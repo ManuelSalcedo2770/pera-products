ff6f29b192454a0d978fb4b3f3c8229b46462a3c
ManuelSalcedo <jsm77250@lasallebajio.edu.mx>
2023-11-14 00:49:24 -0600

    carrousel succeded

--- Pera_Proyecto/js/javascript.js
+++ Pera_Proyecto/js/javascript.js
@@ -1,33 +1,62 @@
-/*
-inspiration
-https://cz.pinterest.com/pin/830703093792161391/
-*/
-var swiper = new Swiper(".swiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 100,
-        modifier: 2.5
-    },
-    keyboard: {
-        enabled: true
-    },
-    mousewheel: {
-        thresholdDelta: 70
-    },
-    spaceBetween: 30,
-    loop: false,
-    breakpoints: {
-        640: {
-            slidesPerView: 2
-        },
-        1024: {
-            slidesPerView: 3
-        }
-    }
+/*----------CLOCK PRODUCTOS--------------------------------------------------------------------------------*/
+window.addEventListener("DOMContentLoaded",() => {
+	const c = new Clock22(".clock");
 });
 
-swiper.slideTo(1, false, false);
+class Clock22 {
+	debug = false;
+	extraTime = 0;
+
+	constructor(el) {
+		this.el = document.querySelector(el);
+
+		this.init();
+	}
+	init() {
+		this.timeUpdate();
+
+		if (this.debug === true) {
+			document.body.addEventListener("click",this.addTime.bind(this,60000));
+		}
+	}
+	get timeAsObject() {
+		let date = new Date();
+		date = new Date(date.getTime() + this.extraTime);
+		const h = date.getHours();
+		const m = date.getMinutes();
+
+		return { h, m };
+	}
+	get timeAsString() {
+		let { h, m } = this.timeAsObject;
+		// prepend 0s if single digits
+		if (h === 0) h = 12;
+		else if (h > 12) h -= 12;
+		if (h < 10) h = `0${h}`;
+		if (m < 10) m = `0${m}`;
+
+		return `${h}:${m}`;
+	}
+	get timeDigits() {
+		return this.timeAsString.replaceAll(":","").split("");
+	}
+	addTime(amount = 0) {
+		this.extraTime += amount;
+		this.timeUpdate();
+	}
+	timeUpdate() {
+		// update the `aria-label`
+		this.el?.setAttribute("aria-label", this.timeAsString);
+		// update the digits
+		this.timeDigits.forEach((digit,i) => {
+			const digitEl = this.el.querySelectorAll("[data-digit]")[i];
+			if (!digitEl) return;
+
+			digitEl.setAttribute("data-digit",digit);
+		});
+		// loop
+		clearTimeout(this.timeUpdateLoop);
+		this.timeUpdateLoop = setTimeout(this.timeUpdate.bind(this),1e3);
+	}
+}
+/*----------CLOCK PRODUCTOS--------------------------------------------------------------------------------*/
